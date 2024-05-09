d9139498457aca1bc554a6704e8929e30352d24f
kohariken <104532358+kohariken@users.noreply.github.com>
2024-05-09 01:01:35 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+document.getElementById('calm').addEventListener('click', function() {
+    alert('Parabéns! Continue mantendo a calma. agora aproveite 😎');
+
+    // Redireciona após o fechamento do alerta.
+    
+    window.location.href = "https://www.youtube.com/watch?v=9gbWg7EGFEc&ab_channel=LuizPoderosoChef%C3%A3o";
+});
+
+document.getElementById('notCalm').addEventListener('click', function() {
+    alert('sente-se e aproveite a agora seu momento de calma 😎');
+
+    // Redireciona após o fechamento do alerta.
+    window.location.href = "https://www.youtube.com/watch?v=9gbWg7EGFEc&ab_channel=LuizPoderosoChef%C3%A3o";
+});
