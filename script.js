7937f5c3d2edf7bcc227a929023539d859165df3
Kohari <104532358+kohariken@users.noreply.github.com>
2024-08-16 06:58:58 -0300

    Update script.js

--- script.js
+++ script.js
@@ -10,5 +10,5 @@ document.getElementById('notCalm').addEventListener('click', function() {
     alert('sente-se e aproveite a agora seu momento de calma 😎');
 
     // Redireciona após o fechamento do alerta.
-    window.location.href = "https://www.youtube.com/watch?v=9gbWg7EGFEc&ab_channel=LuizPoderosoChef%C3%A3o";
+    window.location.href = "https://www.youtube.com/watch?v=xRcWlA1I9z0";
 });
